ca8587b4b112114bb08388c7f2927d4f830aec39
sovnya <enp7s1@tutanota.com>
2023-08-06 00:54:02 +0200

    feat: Add Tech Reborn generator file

--- init.lua
+++ init.lua
@@ -4,9 +4,14 @@ local old_path = package.path
 package.path = package.path .. "/energy_rewrite/?;/energy_rewrite/?.lua;/energy_rewrite/?/init.lua;"
 
 energy.util = require("util")
+
 energy.powah = {
     generator = require("powah.generator"),
     cells = require("powah.cells")
+}
+
+energy.tech_reborn = {
+    generator = require("tech_reborn.generator")
 }
 
 package.path = old_path
---
+++ tech_reborn/generator/init.lua
@@ -0,0 +1,17 @@
+local diesel = require("tech_reborn.generator.diesel")
+local dragon_egg_syphon = require("tech_reborn.generator.dragon_egg_syphon")
+local gas = require("tech_reborn.generator.gas")
+local lightning = require("tech_reborn.generator.lightning")
+local plasma = require("tech_reborn.generator.plasma")
+local solar_panel = require("tech_reborn.generator.solar_panel")
+local water_mill = require("tech_reborn.generator.water_mill")
+
+return {
+    diesel = diesel,
+    dragon_egg_syphon = dragon_egg_syphon,
+    gas = gas,
+    lightning = lightning,
+    plasma = plasma,
+    solar_panel = solar_panel,
+    water_mill = water_mill
+}
