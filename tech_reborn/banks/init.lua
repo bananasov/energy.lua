8d6667edbabee98a56b4496f09cde97cd1838121
sovnya <enp7s1@tutanota.com>
2023-08-06 13:42:06 +0200

    feat(techreborn): Some useful utilities

--- tech_reborn/banks/init.lua
+++ tech_reborn/banks/init.lua
@@ -5,11 +5,40 @@ local adjustable_su = require("tech_reborn.banks.adjustable_su")
 local interdimensional_su = require("tech_reborn.banks.interdimensional_su")
 local lapotronic_su = require("tech_reborn.banks.lapotronic_su")
 
-return {
-    battery_box = battery_box,
-    mfe = mfe,
-    mfsu = mfsu,
-    adjustable_su = adjustable_su,
-    interdimensional_su = interdimensional_su,
-    lapotronic_su = lapotronic_su
+local util = require("util")
+
+local banks = {
+	battery_box = battery_box,
+	mfe = mfe,
+	mfsu = mfsu,
+	adjustable_su = adjustable_su,
+	interdimensional_su = interdimensional_su,
+	lapotronic_su = lapotronic_su,
 }
+
+local types = {
+	"techreborn:low_voltage_su",
+	"techreborn:medium_voltage_su",
+	"techreborn:high_voltage_su",
+	"techreborn:adjustable_su",
+	"techreborn:interdimensional_su",
+	"techreborn:lapotronic_su",
+}
+
+---@return number
+function banks:get_total_stored()
+	local peripherals = util.get_peripherals_from_type(types)
+	local stored = util.get_total_stored_from_type(peripherals)
+
+	return stored
+end
+
+---@return number
+function banks:get_total_capacity()
+	local peripherals = util.get_peripherals_from_type(types)
+	local capacity = util.get_total_capacity_from_type(peripherals)
+
+	return capacity
+end
+
+return banks
--- tech_reborn/generator/solar_panel/init.lua
+++ tech_reborn/generator/solar_panel/init.lua
@@ -1,4 +1,14 @@
+local util = require("util")
+
 local solar = {}
+local types = {
+	"techreborn:basic_solar_panel",
+	"techreborn:advanced_solar_panel",
+	"techreborn:industrial_solar_panel",
+	"techreborn:ultimate_solar_panel",
+	"techreborn:quantum_solar_panel",
+	"techreborn:creative_solar_panel",
+}
 
 solar.basic = require("tech_reborn.generator.solar_panel.basic")
 solar.advanced = require("tech_reborn.generator.solar_panel.advanced")
@@ -6,5 +16,21 @@ solar.industrial = require("tech_reborn.generator.solar_panel.industrial")
 solar.ultimate = require("tech_reborn.generator.solar_panel.ultimate")
 solar.quantum = require("tech_reborn.generator.solar_panel.quantum")
 solar.creative = require("tech_reborn.generator.solar_panel.creative")
+
+---@return number
+function solar:get_total_stored()
+    local peripherals = util.get_peripherals_from_type(types)
+    local stored = util.get_total_stored_from_type(peripherals)
+
+    return stored
+end
+
+---@return number
+function solar:get_total_capacity()
+    local peripherals = util.get_peripherals_from_type(types)
+    local capacity = util.get_total_capacity_from_type(peripherals)
+
+    return capacity
+end
 
 return solar
