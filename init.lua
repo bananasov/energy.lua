27567f0a637053132e8146e5fa1210378e2bf2cb
sovnya <enp7s1@tutanota.com>
2023-08-12 19:36:00 +0200

    fix(powah): Make `id` parameter a number

--- init.lua
+++ init.lua
@@ -37,7 +37,7 @@ end
 ---@param name string
 function energy.powah.get_machine_type(name)
 	local machine, machine_type, id = string.match(name, "^powah:(.-)_(.-)_(.+)$")
-	return machine, machine_type, id
+	return machine, machine_type, tonumber(id)
 end
 
 package.path = old_path
