a2f020626f7229f8c9780dec195f7e42d0124501
sovnya <enp7s1@tutanota.com>
2023-08-12 17:13:42 +0200

    feat: experimental path stuff

--- init.lua
+++ init.lua
@@ -1,32 +1,37 @@
 local energy = {}
 
 local old_path = package.path
-package.path = package.path .. "/energy/?;/energy/?.lua;/energy/?/init.lua;"
+local sourcePath = fs.getDir(select(2, ...) or "")
+
+package.path = string.format(
+    package.path .. "/%s/?;/%s/?.lua;/%s/?/init.lua;",
+    sourcePath, sourcePath, sourcePath
+)
 
 energy.util = require("util")
 
 energy.powah = {
-    generator = require("powah.generator"),
-    cells = require("powah.cells")
+	generator = require("powah.generator"),
+	cells = require("powah.cells"),
 }
 
 energy.tech_reborn = {
-    generator = require("tech_reborn.generator"),
-    banks = require("tech_reborn.banks")
+	generator = require("tech_reborn.generator"),
+	banks = require("tech_reborn.banks"),
 }
 
 ---@param name string The peripheral name
 ---@return number
 function energy.get_stored(name)
-    local wrap = peripheral.wrap(name)
-    return wrap.getEnergy()
+	local wrap = peripheral.wrap(name)
+	return wrap.getEnergy()
 end
 
 ---@param name string The peripheral name
 ---@return number
 function energy.get_capacity(name)
-    local wrap = peripheral.wrap(name)
-    return wrap.getEnergyCapacity()
+	local wrap = peripheral.wrap(name)
+	return wrap.getEnergyCapacity()
 end
 
 package.path = old_path
